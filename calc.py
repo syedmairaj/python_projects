b64bbe2477113f659626f4e45927fce631822e37
syedayaan <sydayaan0@gmail.com>
2023-09-30 16:25:10 +0530

    calc.py

---
+++ calc.py
@@ -0,0 +1,20 @@
+## calculator for 2 numbers
+
+
+inp_1 = int(input("1) "))
+func = input('enumerate:, +, -, *,/ \n' )
+inp_2 = int(input("2) "))
+if func == '+':
+    print(inp_1 + inp_2)
+    
+elif func == '*':
+    print(inp_1 *inp_2)
+    
+elif func == '-':
+    print(inp_1-inp_2)
+    
+elif func == '/':
+    print(inp_1/inp_2)
+    
+else:
+    print("Please input a number")
